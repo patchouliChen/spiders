149dbf0a5a539150304a9e561230016f82e257fc
patchouliChen <cxm0225@175game.com>
2017-04-20 16:24:05 +0800

    move image hosts and filters to another file

--- cl/main.py
+++ cl/main.py
@@ -6,6 +6,8 @@ import requests
 import datetime
 sys.path.append("../")
 from utils import *
+from image_hosts import image_hosts
+from image_hosts import filters
 
 class CL:
     def __init__(self):
@@ -37,47 +39,6 @@ class CL:
         return links
 
     def get_image_url(self, page_code):
-        image_hosts = [
-                "http://www.datuimg.com/images.*?.[jpg|jpeg|gif]",
-                "http://img.*?.uploadhouse.com/fileuploads.*?.[jpg|jpeg|gif]",
-                "http://img.*?.showhaotu.com.*?.[jpg|jpeg|gif]",
-                "http://img.*?.showhaotu.xyz.*?.[jpg|jpeg|gif]",
-                "https://img.*?.showhaotu.com.*?.[jpg|jpeg|gif]",
-                "http://s1.s8tu.com.*?.[jpg|jpeg|gif]",
-                "http://p2.imageab.com.*?.[jpg|jpeg|gif]",
-                "http://.*?ultraimg.com/images.*?.[jpg|jpeg|gif]",
-                "http://img.*?.imagetwist.com.*?.[jpg|jpeg|gif]",
-                "http://.*?tinypic.com.*?.[jpg|jpeg|gif]",
-                "http://.*?chuantupian.com.*?.[jpg|jpeg|gif]",
-                "http://.*?imgur.com.*?.[jpg|jpeg|gif]",
-                "https://.*?media.tumblr.com.*?.[jpg|jpeg|gif]",
-                "http://.*?imagebam.com.*?.[jpg|jpeg|gif]",
-                "http://.*?kepkezelo.com/images.*?.[jpg|jpeg|gif]",
-                "http://.*?directupload.net.*?.[jpg|jpeg|gif]",
-                "http://.*?freeimage.us/image.php?id=",
-                "http://.*?picpar.com.*?.[jpg|jpeg|gif]",
-                "http://.*?imgaa.com.*?.[jpg|jpeg|gif]",
-                "http://.*?imagefruity.com.*?.[jpg|jpeg|gif]",
-                "http://.*?ihostimg.com.*?.[jpg|jpeg|gif]",
-                "http://.*?imgspice.com.*?.[jpg|jpeg|gif]",
-                "http://.*?anony.ws.*?.[jpg|jpeg|gif]",
-                "http://.*?imgs.co.*?.[jpg|jpeg|gif]",
-                "http://imgpr0n.com.*?.[jpg|jpeg|gif]",
-                "http://urpi.cz.*?.[jpg|jpeg|gif]",
-                "http://.*?img-central.com.*?.[jpg|jpeg|gif]",
-                "http://.*?fullpix.net.*?.[jpg|jpeg|gif]",
-                "http://.*?qpic.ws.*?.[jpg|jpeg|gif]",
-                "https://.*?imgs.co.*?.[jpg|jpeg|gif]",
-
-                "http://.*?imagestorming.com/images.*?.[jpg|jpeg|gif]",
-                "http://.*?bigtox.com.*?.[jpg|jpeg|gif]",
-                "http://.*?ninja.*?.[jpg|jpeg|gif]",
-                "http://.*?showss.net.*?.[jpg|jpeg|gif]",
-                "http://.*?lookpic.com.*?.[jpg|jpeg|gif]",
-                "http://.*?uploadfr.com.*?.[jpg|jpeg|gif]",
-                "http://.*?gagr.com.*?.[jpg|jpeg|gif]",
-                "http://.*?1024.photos.*?.[jpg|jpeg|gif|png]",
-        ]
         pattern = "src='(" + "|".join(image_hosts) + ".*?)'"
         r = re.compile(pattern, re.S)
         return r.findall(page_code)
@@ -131,17 +92,6 @@ class CL:
         f.close()
 
     def filterURL(self, image_url):
-        filters = [
-            "www.imagestorming.com",
-            "bigtox.com",
-            "ninja",
-            "showss.net",
-            "lookpic.com",
-            "uploadfr.com",
-            "gagr.com",
-            "1024.photos",
-        ]
-
         for f in filters:
             if image_url.find(f) != -1:
                 return True 
